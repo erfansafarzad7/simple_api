d5367b023057fc6901b59b54feceb678b986b3a8
Erfan <erfansafarzad7@gmail.com>
2024-02-12 18:39:23 +0330

    Generics

--- stuff/views.py
+++ stuff/views.py
@@ -1,34 +1,13 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 from stuff.models import Stuff
 from stuff.serializers import StuffSerializer
 
 
-class StuffList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+class StuffList(generics.ListCreateAPIView):
     queryset = Stuff.objects.all()
     serializer_class = StuffSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class StuffDetail(mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.DestroyModelMixin,
-                  generics.GenericAPIView):
-
+class StuffDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Stuff.objects.all()
     serializer_class = StuffSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
